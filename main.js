8c99ae1db937ca785c7e8809fd551fdf09d39d71
Jordan Rosas <jordanrosas1203@gmail.com>
2018-11-26 15:43:52 -0600

    completed cookout practice lesson.

---
+++ main.js
@@ -0,0 +1,40 @@
+const hamburger = {
+	name: 'Hamburger',
+	type: 'beef',
+	cooked: false,
+}
+const zucchini = {
+	name: 'Zucchini',
+	type: 'vegetable',
+	cooked: false,
+}
+const chickenBreast = {
+	name: 'Chicken Breast',
+	type: 'chicken',
+	cooked: false,
+}
+const corn = {
+	name: 'Corn',
+	type: 'vegetable',
+	cooked: false,
+}
+const steak = {
+	name: 'Steak',
+	type: 'beef',
+	cooked: false,
+}
+
+// An first array containing the objects to be cooked.
+const uncookedFood = [hamburger, zucchini, chickenBreast, corn, steak];
+
+// An empty array that will store the objects after the `grill()` function cooks the food.
+const cookedFood = [];
+
+function grill (foodArray) {
+    for(var i = 0; i < foodArray.length; i++){
+        foodArray[i].cooked = true;
+        cookedFood.push(foodArray[i]);
+        console.log(foodArray);
+    }
+};
+grill(uncookedFood)
